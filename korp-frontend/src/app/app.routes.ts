5b091aa05b4547305b9053ca323221671ad14278
Isaque Roberto <isaquezulato@gmail.com>
2026-09-09 16:11:48 -0300

    🔨refactor: Configuring the Product Route Connection

--- korp-frontend/src/app/app.routes.ts
+++ korp-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProdutosComponent } from './components/produtos/produtos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'produtos', component: ProdutosComponent },
+];
