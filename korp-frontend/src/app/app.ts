9e94be600c2df8e57f4db87a87a3af64897b7ffe
Isaque Roberto <isaquezulato@gmail.com>
2026-09-09 18:10:49 -0300

    💄   style: Implementing the top navigation bar

--- korp-frontend/src/app/app.ts
+++ korp-frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('korp-frontend');
 }
+
