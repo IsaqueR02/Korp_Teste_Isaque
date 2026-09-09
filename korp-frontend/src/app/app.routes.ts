511a299b55d8134803e7d08c4b462ba7fdba2694
Isaque Roberto <isaquezulato@gmail.com>
2026-09-09 18:02:08 -0300

    🔨refactor: configuring application routes

--- korp-frontend/src/app/app.routes.ts
+++ korp-frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProdutosComponent } from './components/produtos/produtos';
+import { NotasFiscaisComponent } from './components/notas-fiscais/notas-fiscais';
 
 export const routes: Routes = [
-    { path: 'produtos', component: ProdutosComponent },
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'notas-fiscais', component: NotasFiscaisComponent },
 ];
+
